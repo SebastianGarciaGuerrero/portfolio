a28aaa72441429248b01c8f8b68f4d7412be2895
Sebastian Garcia Guerrero <seba.garcia.g@gmail.com>
2024-08-02 19:22:05 -0400

    hero section

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,22 +1,60 @@
+"use client";
 import React from "react";
 import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
 
 export const HeroSection = () => {
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-12">
-        <div className="col-span-7 place-self-center">
-          <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
-            Sebastián García
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <div className="col-span-7 place-self-center text-center sm:text-left">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+              ¡Hola! Me llamo {""}
+            </span>
+            <br />
+            <TypeAnimation
+              sequence={[
+                "Sebastián García",
+                1000,
+                "Desarrollador Web",
+                1000,
+                "Programador",
+                1000,
+                "Eterno Aprendiz",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
           </h1>
-          <p className="text-[#ADB7BE] text-lg lg:text-xl">
+          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
             Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Necessitatibus possimus officia corrupti culpa quibusdam, molestiae
             vel. Quisquam, quos. Quisquam, quos.
           </p>
-        </div> 
-        <div className="col-span-5">
-            <Image src="/images/hero-image.png"></Image>
+          <div>
+            <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+              Contratame ctm
+            </button>
+            <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-600 hover:bg-slate-800 text-white mt-3">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+                Descarga el CV aqui
+              </span>
+            </button>
+          </div>
+        </div>
+        <div className="col-span-5 place-self-center mt-4 lg:mt-o">
+          <div className="rounded-full bg-[#181818] w-[500px] h-[500px] lg:w-[400px] relative">
+            <Image
+              src="/image/seba3d.png"
+              alt="hero-image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={500}
+              height={500}
+            />
+          </div>
         </div>
       </div>
     </section>
