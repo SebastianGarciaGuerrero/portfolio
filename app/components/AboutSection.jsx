f091d1940540e8d4a6ed37cddd503263db87851c
Sebastian Garcia Guerrero <seba.garcia.g@gmail.com>
2024-08-05 12:04:04 -0400

    about section

---
+++ app/components/AboutSection.jsx
@@ -0,0 +1,89 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul>
+        <li>JavaScript</li>
+        <li>Node.js</li>
+        <li>React.js</li>
+        <li>Next.js</li>
+        <li>PostgreSQL</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Educación",
+    id: "educacion",
+    content: (
+        <ul>
+            <li>IACC</li>
+            <li>Desafio Latam</li>
+            <li>YouTube</li>
+        </ul>
+        ),
+  },
+  {
+    title: "Certificados",
+    id: "certificados",
+    content: (
+        <ul>
+            <li>FullStack Developer</li>
+            <li>Cisco</li>
+        </ul>
+        ),
+  }
+];
+
+export const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/image/cycle.jpg" alt="cycle" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base md:text-lg">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus
+            repudiandae, id modi quisquam earum odit atque incidunt fugiat
+            aliquam itaque?
+          </p>
+          <div className=" flex flex-row mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {""}Skills{""}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("educacion")}
+              active={tab === "educacion"}
+            >
+              {""}Educación{""}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certificados")}
+              active={tab === "certificados"}
+            >
+              {""}Certificados{""}
+            </TabButton>
+          </div>
+          <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
+        </div>
+      </div>
+    </section>
+  );
+};
